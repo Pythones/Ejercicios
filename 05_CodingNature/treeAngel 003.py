ece58d13a3c88d92000be449e314e27e048ebf44
angellinares <angellinaresgarcia@gmail.com>
2013-02-17 21:06:42 +0100

    New algorithm improved.
    Added fruits.

---
+++ 05_CodingNature/treeAngel 003.py
@@ -0,0 +1,27 @@
+#Testing new algorithms based on screensaver logic.
+
+import rhinoscriptsyntax as rs
+import math as m
+import random as r
+i = 0
+
+def main():
+    #asking for data.
+    dblLength = rs.GetReal("Initial growing length",1,0.01,10)
+    dblAngle = m.radians(rs.GetReal("Random angle limit",10,0,90))
+    strPt = rs.GetPoint("Growing point")
+    intLimit = rs.GetInteger("Iterations limit",20,1,1000)
+    tree(strPt,dblLength, dblAngle, intLimit, i)
+
+def tree(Pto,dblStep,dblAngle,intLimit,x):
+    if x>intLimit: return
+    dblRand = r.uniform(-dblAngle,dblAngle)
+    v3dRand = (m.cos(dblRand+(m.pi/2)),m.sin(dblRand+(m.pi/2)),0)
+    PtM = rs.PointAdd(Pto,v3dRand)
+    rs.AddLine(Pto,PtM)
+    intBranch = r.randint(1,5)
+    x+=1
+    for x in range(1,intBranch):
+        tree(PtM,dblStep,dblAngle*1.1,intLimit*0.5,x+1)
+
+main()
