586f4e26ce95c23c1045a987f580b5a831954709
manu <manuelosuna@gmail.com>
2013-05-17 21:23:48 +0200

    Update

--- 10_ProjectDeep/130517_Lineweight_closest.py
+++ 10_ProjectDeep/130517_Lineweight_closest.py
@@ -9,6 +9,7 @@ def imput():
 
     #Creamos un plano sobre el que proyectar
     PlProy = rs.WorldYZPlane()
+    PlFinal = rs.GetObject("Dame algo sobre lo que proyectar")
     #Pedimos la volumetria y obtenemos sus lineas
     edge = []
     paraproyectar = rs.GetObject("Dame algo que proyectar")
@@ -40,12 +41,46 @@ def imput():
         else:
             distMin.append(distPt2[i])
             
+    #Interpretamos si es cerca o lejos
+    lineMod = rs.GetObject("Dame linea de referencia")
+    refMod = rs.CurveLength(lineMod)
+    
+    #Comparamos las distancias
+    farclose = []
+    for i in range(len(edge)):
+        if distMin[i]<refMod:
+            farclose.append(True)
+        else:
+            farclose.append(False)
+            
             
             
     #Comprobamos a que hemos llegado
-    for i in range(len(distMin)):
-        rs.AddTextDot("PtMin",distMin[i])
+    #for i in range(len(distMin)):
+        #rs.AddTextDot("PtMin",distMin[i])
     
-    print (len(distMin))
+    print (farclose)
+    #deeper(PlProy,edge,farclose)
     
-input()
+    #User comunication module 
+    #def deeper(plane,lines,dist):
+    
+    #Proyectamos las lineas
+    Proj = []
+    Proj = rs.ProjectCurveToSurface(edge,PlFinal,(1,0,0))
+    
+    #Damos espesor a la proyeccion
+    for i in range(len(Proj)):
+        if farclose[i] is True:
+            #rs.AddLayer("Deep_Cerca",Red)
+            rs.ObjectColor(Proj[i],(100,0,0))
+            
+
+#Eliminamos las lineas ocultas de nuestra proyeccion            
+def hiddenlines():
+    
+    #hacemos make2d
+    
+    
+    
+imput()
